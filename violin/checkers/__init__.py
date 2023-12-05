b0e82aae2ab5aef9c5cf72b0dbae89855e2b16ff
mainuser <nikitakakurnikov@gmail.com>
2023-12-05 22:42:50 +0000

    cleanup

--- violin/checkers/__init__.py
+++ violin/checkers/__init__.py
@@ -1,3 +1,3 @@
 from .base import is_transform_instance, is_dir, is_file, is_not_empty_str, is_dict, is_boolflag, is_str_not_empty, is_int, is_positive, is_not_negative
 from .generators import nullable, unite
-from .generated import is_positive_int
+from .generated import is_positive_int, is_not_negative_int
